04e7861853ea7cb703b38946591b706be2dde904
vagrant <vagrant@vagrant.vm>
2019-05-11 07:31:48 +0000

    Adds descending sort to query1 so most views is on top

--- query1.sql
+++ query1.sql
@@ -7,6 +7,11 @@ order by num_views desc
 limit 3;
 
 create or replace view slugs as
-select substring(log_path_counts.path from '\/article\/(.*)') as slug, num_views from log_path_counts;
+select substring(log_path_counts.path
+from '\/article\/(.*)') as slug, num_views
+from log_path_counts;
 
-select title, cast(num_views as varchar(64)) || ' views' as views from slugs join articles on articles.slug = slugs.slug;
+select title, cast(num_views as varchar(64)) || ' views' as views
+from slugs
+join articles on articles.slug = slugs.slug
+order by num_views desc;
