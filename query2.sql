20167c2d941700e8e9dbd418693314ece13ec215
vagrant <vagrant@vagrant.vm>
2019-05-11 07:30:43 +0000

    Significantly refactors query2 to sum views for authors into single result

--- query2.sql
+++ query2.sql
@@ -1,14 +1,19 @@
-WITH x AS (
-select count(path) || ' views' as num_views, path
-from log
-where log.path like '/article/%'
-group by path
+WITH y AS (
+  WITH x AS (
+    select count(path) as num_views, path
+    from log
+    where log.path like '/article/%'
+    group by path
+  )
+  select SUM(num_views) as total_views, articles.author
+  from x
+  join articles on
+    substring(x.path from '\/article\/(.*)') = articles.slug
+  group by articles.author
 )
-select authors.name, num_views
-from x
-join articles on
-  substring(x.path from '\/article\/(.*)') = articles.slug
-join authors on
-  articles.author = authors.id
-group by authors.name, num_views
-order by num_views asc;
+select authors.name, y.total_views
+from authors
+join y on
+  y.author = authors.id
+group by authors.name, y.total_views
+order by y.total_views desc;
